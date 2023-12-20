4c13b90e7b1ab9fe803ce53a46149832a692eca9
Elon Thy <Chanvuthy.leap@student.cadt.edu.kh>
2023-12-20 16:53:45 +0700

    add:lesson3 useState

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import Home from './pages/Home';
 import Navbar from './components/layout/Navbar';
+import { useState } from 'react';
 
 function App() {
+
+  const [name , setName] = useState('vuthy');
+  const handleChange = () => {
+    setName('tiven');
+  }
+
   return (
     <div className="App">
       <Navbar/>
-        <h2>Hello this is React</h2>
+      <h4>{name}</h4> 
+      <button onClick={handleChange}>change name</button> 
       <Home/>
     </div>
   );
 }
 
 export default App;
+
