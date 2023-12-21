f335d8755a9402b8ee00ffe81f9bca3b792eac68
Elon Thy <Chanvuthy.leap@student.cadt.edu.kh>
2023-12-21 23:43:37 +0700

    add:lesson8

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,17 +1,43 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 const Create = () => {
+    const [title , setTitle] = useState('');
+    const [body , setBody] = useState('');
+    const [author , setAuthor] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const article = {title , body , author} ;
+        fetch ('http://localhost:4000/article' , {
+            method : 'POST',
+            headers : {"Content-Type" : "application/json"},
+            body : JSON.stringify(article)
+        })
+        .then((data) => {
+            console.log('Data is added' , data);
+            navigate('/');
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+
+    }
+
+
     return ( 
-        <form className="custom-form">
+        <form onSubmit={handleSubmit} className="custom-form">
             <label >
                 Title :
-                <input type="text"  />
+                <input type="text" value={title} onChange={((e) => setTitle(e.target.value))}    />
             </label>
             <label >
                 Body :
-                <textarea ></textarea>
+                <textarea value={body} onChange={((e) => setBody(e.target.value))} />
             </label>
            <label >
             Author :
-            <select >
+            <select value={author} onChange={((e) => setAuthor(e.target.value))}>
                     <option value="Leap Chanvuthy">Leap Chanvuthy</option>
                     <option value="John Doe">John Doe</option>
                     <option value="Jame Henderson">Jame Henderson</option>
